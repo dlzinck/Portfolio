caa2ffedbe232a5d86ae587f86f9d563ef2d408e
Drew Zinck <dlzincktheii@gmail.com>
2023-01-11 18:45:24 -0700

    Deploy React app to GitHub Pages

---
+++ precache-manifest.7c3f0a2b8f9de2004a2f93f46203b7a1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "912e6d221cdd72f86cde1295c71745e6",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "e47ca4a398fedb921e49",
+    "url": "/Portfolio/static/css/main.44727169.chunk.css"
+  },
+  {
+    "revision": "98601ac5a57ae51f4fd5",
+    "url": "/Portfolio/static/js/2.0c4d01c0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Portfolio/static/js/2.0c4d01c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e47ca4a398fedb921e49",
+    "url": "/Portfolio/static/js/main.28378840.chunk.js"
+  },
+  {
+    "revision": "db1ea67b8a08076c9f67",
+    "url": "/Portfolio/static/js/runtime-main.0d26a884.js"
+  },
+  {
+    "revision": "003bc9ee005ea79a060cbe2d9b89b2fa",
+    "url": "/Portfolio/static/media/Away.003bc9ee.jpg"
+  },
+  {
+    "revision": "d8aec59427bd2512ef1f4584fc49c580",
+    "url": "/Portfolio/static/media/Password-Generator.d8aec594.jpg"
+  },
+  {
+    "revision": "4c7658dfbe9fa47d53602e0ff94c5410",
+    "url": "/Portfolio/static/media/Text-Editor-(PWA).4c7658df.jpg"
+  },
+  {
+    "revision": "4eed8f71a69bc7f9e3682c112da96511",
+    "url": "/Portfolio/static/media/Work-Day-Scheduler.4eed8f71.jpg"
+  },
+  {
+    "revision": "07770787f6241705279dd6ad40ea0bb1",
+    "url": "/Portfolio/static/media/portfolio-header.07770787.jpg"
+  },
+  {
+    "revision": "4995a6a5ae1f5328526a26612d61f545",
+    "url": "/Portfolio/static/media/profilepic.4995a6a5.jpg"
+  }
+]);
